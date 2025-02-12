a20e0e3d363d723b0c3d5393897222a66288eccf
Marko Marković <okram@civokram.com>
2025-02-12 15:21:49 +0400

    feat: Add support for plain, and conventional commit messages
    Introduces an option to generate commit messages following the Conventional Commits guidelines. Users can specify the type of commit message through the `MAI_COMMIT_TYPE` environment variable, which can be set to either "PLAIN" or "CONVENTIONAL". If an invalid value is provided, it defaults to "PLAIN". This change enhances flexibility in commit message formatting without altering existing behavior for those who prefer plain messages.

--- functions/mai-commit.fish
+++ functions/mai-commit.fish
@@ -1,6 +1,23 @@
 
 function mai-commit --description "Generate a git commit message based on staged changes"
 
+    # Types of commits supported:
+    # - PLAIN: Just the subject and body, no special formatting
+    # - CONVENTIONAL: https://www.conventionalcommits.org/en/v1.0.0/
+    if not set --query MAI_COMMIT_TYPE
+        set --function MAI_COMMIT_TYPE "PLAIN"
+    else if not string match -r '^(PLAIN|CONVENTIONAL)$' $MAI_COMMIT_TYPE
+        echo "Invalid MAI_COMMIT_TYPE: $MAI_COMMIT_TYPE. Using PLAIN instead." >&2
+        set --function MAI_COMMIT_TYPE "PLAIN"
+    end
+    echo "Generating a $MAI_COMMIT_TYPE type commit message" >&2
+    switch $MAI_COMMIT_TYPE
+        case "PLAIN"
+            set MAI_COMMIT_TYPE 1
+        case "CONVENTIONAL"
+            set MAI_COMMIT_TYPE 2
+    end
+
     argparse h/help e/edit a/amend -- $argv
 
     # Display help and exit
@@ -32,7 +49,8 @@ function mai-commit --description "Generate a git commit message based on staged
         return 1
     end
 
-    set --local PROMPT "
+    # PLAIN
+    set --local PROMPT[1] "
 You are an expert software engineer.
 Review the provided context and diffs which are about to be committed to a git repo.
 Reply with JUST the commit message, without quotes, comments, questions, etc!
@@ -42,11 +60,25 @@ Write clear, concise, and descriptive TITLE that explains the MAIN GOAL of the c
 
 Generate a commit message for those changes following these rules:
 - Use imperative tense and active voice.
-- Structure SUBJECT as follows: <TYPE>(optional SCOPE): <TITLE>
 - TITLE should be clear, concise, shorter than 72 characters if possible.
+- Explain breaking changes in the DESCRIPTION section.
+
+After SUBJECT line, add a DESCRIPTION if necessary.
+- Optional and can provide more context about the change.
+- Write in present tense, active voice, imperative mood.
+- Explain what was changed, why it was necessary with motivation, contrasting previous behavior.
+- Do NOT describe how the change was made.
+- Separate description using two new lines.
+"
+
+    # CONVENTIONAL
+    set --local PROMPT[2] "
+$PROMPT[1]
+
+Follow Conventional Commits guidelines:
+- Structure SUBJECT as follows: <TYPE>(optional SCOPE): <TITLE>
 - SCOPE is optional and provides context, not a file name.
 - Indicate breaking changes with an exclamation mark ! before the colon : in the SUBJECT line.
-- Explain breaking changes in the DESCRIPTION section.
 
 TYPEs:
 feat: Add or remove a new feature
@@ -59,15 +91,9 @@ docs: Affect documentation only
 build: Affect build tools, ci pipeline, dependencies, version, ...
 ops: Affect infrastructure, deployment, backup, recovery, ...
 chore: Miscellaneous changes e.g. modifying .gitignore
-
-After SUBJECT line, add a DESCRIPTION if necessary.
-- Optional and can provide more context about the change.
-- Write in present tense, active voice, imperative mood.
-- Explain what was changed, why it was necessary with motivation, contrasting previous behavior.
-- Do NOT describe how the change was made.
-- Separate description using two new lines.
 "
 
+    set --local PROMPT $PROMPT[$MAI_COMMIT_TYPE]
     set --local MESSAGE (ollama run $MAI_OLLAMA_MODEL "$PROMPT\n\n$GIT_DIFF" | string collect)
     set --local TMP_MESSAGE_FILE (mktemp)
     echo "$MESSAGE" > $TMP_MESSAGE_FILE
